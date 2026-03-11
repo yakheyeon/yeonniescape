7c3b0813560359bff3feb08599791b349e228a9d
yakheyeon <123567852+yakheyeon@users.noreply.github.com>
2026-03-12 01:21:35 +0900

    Update service worker for cache versioning and events

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,31 @@
-const CACHE_NAME = "escape-cache-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const CACHE_NAME="escape-v2";
+
+const FILES=[
+"./",
+"./index.html",
+"./manifest.json"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+self.addEventListener("install",event=>{
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES))
+);
+self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("activate",event=>{
+event.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+);
+})
+);
+self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+event.respondWith(
+caches.match(event.request).then(res=>res||fetch(event.request))
+);
 });
